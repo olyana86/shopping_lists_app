5cfcf9e6c16da6be6c3ec3cc8f7090858802faf3
AilaTS <105786909+AilaTS@users.noreply.github.com>
2023-10-06 09:56:51 +0300

    MainActivity с findViewById

--- app/src/main/java/com/example/shoppinglistsapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistsapp/presentation/activity/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.shoppinglistsapp.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglistsapp.R
+import com.example.shoppinglistsapp.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+
+private lateinit var binding: ActivityMainBinding
+lateinit var userListsRecyclerView: RecyclerView
+lateinit var categoriesRecyclerView: RecyclerView
+lateinit var addNewListFab: ExtendedFloatingActionButton
+lateinit var firstPriorityBtn: MaterialButton
+lateinit var secondPriorityBtn: MaterialButton
+lateinit var thirdPriorityBtn: MaterialButton
+lateinit var allCategoriesBtn: MaterialButton
+lateinit var allPlacesToBuyBtn: MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userListsRecyclerView = findViewById(R.id.all_user_lists_main_recyclerview)
+        categoriesRecyclerView = findViewById(R.id.all_categories_main_recyclerview)
+        addNewListFab = findViewById(R.id.add_new_list_fab)
+        firstPriorityBtn = findViewById(R.id.first_priority_btn)
+        secondPriorityBtn = findViewById(R.id.second_priority_btn)
+        thirdPriorityBtn = findViewById(R.id.third_priority_btn)
+        allCategoriesBtn = findViewById(R.id.all_categories_btn)
+        allPlacesToBuyBtn = findViewById(R.id.all_places_to_buy_btn)
+
     }
 }
--- app/src/main/java/com/example/shoppinglistsapp/presentation/activity/NewUserListActivity.kt
+++ app/src/main/java/com/example/shoppinglistsapp/presentation/activity/NewUserListActivity.kt
@@ -2,7 +2,14 @@ package com.example.shoppinglistsapp.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.shoppinglistsapp.R
+
+lateinit var listTitleEditText: EditText
+lateinit var readyBtn: Button
+lateinit var manageListBtn: Button
+
 
 class NewUserListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
